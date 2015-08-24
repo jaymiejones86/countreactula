e7e3bd0af1780bc4e4135c0ecf93944cecd40342
Matt Patterson <matt.patterson94@gmail.com>
2015-08-24 15:55:00 +1000

    Update default config and readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-module: {
-  loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel'
-    }
-  ]
-}
+module.exports = {
+  cache: true,
+  entry: './js/index.js',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel'},
+      { test: /\.jsx$/, loader: 'babel'}
+    ]
+  }
+};
